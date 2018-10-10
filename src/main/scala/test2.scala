169e0ae9758ca1075e5973659524738f4901162d
Admin <alex.salako26@gmail.com>
2018-10-10 11:26:53 +0100

    Scala exercises (Basic)

--- src/main/scala/test2.scala
+++ src/main/scala/test2.scala
@@ -1,3 +1,16 @@
+import scala.io.StdIn.readLine
+import scala.collection.mutable.Stack
+
 object test2 extends App {
-  println("World")
-}
+
+  var greeting = "Hello "
+  val input = readLine("What is your name?  ")
+
+  def switch(num1: Int, num2: Int) = {
+    var ints = Stack[Int]()
+  }
+
+
+  }
+
+
